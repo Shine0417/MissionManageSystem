5e837ef8fdab21a9231f1f4c7df96ca328b0167e
Shine0417 <b07902109@ntu.edu.tw>
2021-06-03 23:30:24 +0800

    done 11, 12 start deploying 13 heroju

--- db/migrate/20210523080647_create_missions.rb
+++ db/migrate/20210523080647_create_missions.rb
@@ -1,8 +1,8 @@
 class CreateMissions < ActiveRecord::Migration[6.1]
   def change
     create_table :missions do |t|
-      t.string :title
-      t.text :description
+      t.string :title, null: false
+      t.text :description, null: false
 
       t.timestamps
     end

1148e38a329935daecd193d7f70aae928ee24c4f
Shine0417 <b07902109@ntu.edu.tw>
2021-06-08 02:48:05 +0800

    fix open redirection

--- app/models/mission.rb
+++ app/models/mission.rb
@@ -5,5 +5,5 @@ class Mission < ApplicationRecord
   validates_uniqueness_of :title, case_sensitive: true
 
   scope :title_like, ->(title) { where('title LIKE ?', "%#{title}%") if title }
-  scope :status, ->(status) { where({ status: status }) if status.present? && Mission.statuses.keys.include?(status) }
+  scope :status, ->(status) { where({ status: status }) if Mission.statuses.keys.include?(status) }
 end

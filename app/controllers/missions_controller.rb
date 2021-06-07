05d9eeb12ea1057cda281e4a21c12f6246d766da
Shine0417 <b07902109@ntu.edu.tw>
2021-06-08 02:48:05 +0800

    merge open redirection

--- app/controllers/missions_controller.rb
+++ app/controllers/missions_controller.rb
@@ -21,8 +21,8 @@ class MissionsController < ApplicationController
 
   def update
     if @mission.update(mission_params)
-      if params[:redirect]
-        redirect_to({ action: params[:redirect] }, notice: I18n.t(:update_mission, scope: :notice))
+      if params[:redirect].present?
+        redirect_back fallback_location: mission_path(@mission), notice: I18n.t(:update_mission, scope: :notice)
       else
         redirect_to mission_path(@mission), notice: I18n.t(:update_mission, scope: :notice)
       end
--- app/models/mission.rb
+++ app/models/mission.rb
@@ -3,4 +3,7 @@ class Mission < ApplicationRecord
   enum priority: { low: 0, medium: 1, high: 2 }
   validates :title, :description, :status, :priority, presence: true
   validates_uniqueness_of :title, case_sensitive: true
+
+  scope :title_like, ->(title) { where('title LIKE ?', "%#{title}%") if title }
+  scope :status, ->(status) { where({ status: status }) if Mission.statuses.keys.include?(status) }
 end
